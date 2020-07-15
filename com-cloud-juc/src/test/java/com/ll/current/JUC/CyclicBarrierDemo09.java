889953ba0a627c3bb8d5182b886ed20c9dde4c91
wink60 <wink60@163.com>
2020-07-15 15:17:47 +0800

    JUC  等够7人吃饭

---
+++ com-cloud-juc/src/test/java/com/ll/current/JUC/CyclicBarrierDemo09.java
@@ -0,0 +1,37 @@
+package com.ll.current.JUC;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * @ Author     ：liul
+ * @ Date       ：Created in 10:40 2020/7/6
+ * @ Description：${
+ *      等够7人开饭
+ * }
+ * @ Modified By：
+ * @Version: $1.00$
+ */
+public class CyclicBarrierDemo09 {
+    public static void main(String[] args)throws ExecutionException ,InterruptedException {
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(7,()->{
+            System.out.println("等够7人开饭");
+        });
+        for (int i = 0; i <7 ; i++) {
+            final int tempInt=i;
+            new Thread(()->{
+                System.out.println(Thread.currentThread().getName()+"\t 第"+tempInt+"个人已经到了");
+                try {
+                    cyclicBarrier.await();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (BrokenBarrierException e) {
+                    e.printStackTrace();
+                } finally {
+                }
+            },String.valueOf(i)).start();
+        }
+    }
+}
