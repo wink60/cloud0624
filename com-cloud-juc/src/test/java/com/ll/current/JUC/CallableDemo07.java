39d02b09a0229bddc6069c5e530b07350eab037a
wink60 <wink60@163.com>
2020-07-15 15:05:04 +0800

    JUC 线程的第三种方法

--- com-cloud-juc/src/test/java/com/ll/current/JUC/CallableDemo07.java
+++ com-cloud-juc/src/test/java/com/ll/current/JUC/CallableDemo07.java
@@ -2,10 +2,7 @@ package com.ll.current.JUC;
 
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
+import java.util.concurrent.*;
 
 class MyThread1 implements Runnable{
 
@@ -18,7 +15,8 @@ class MyThread2 implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
-        System.out.println("Come in Callable!");
+        System.out.println("Come in Callable!***********");
+        TimeUnit.SECONDS.sleep(4);
         return 1024;
     }
 }
@@ -27,7 +25,11 @@ class MyThread2 implements Callable<Integer> {
  * @ Author     ：liul
  * @ Date       ：Created in 10:40 2020/7/6
  * @ Description：${
+ *       多线程，第三种方法Callable
+ *       不同与Runnable 的三点：call/run 有返回值 有异常
  *
+ *       1.get 方法一般放在最后一行。
+ *       2.Callable 调用两次计算，相同的有缓存
  * }
  * @ Modified By：
  * @Version: $1.00$
@@ -36,11 +38,14 @@ public class CallableDemo07 {
     public static void main(String[] args)throws ExecutionException ,InterruptedException{
         /*Thread thread1 = new Thread(new MyThread1(),"AAA" );
         thread1.start();*/
+        //资源类
         FutureTask<Integer> futureTask = new FutureTask(new MyThread2());
 
         new Thread(futureTask,"BB").start();
-        Integer integer = futureTask.get();
-        System.out.println(integer);
+        new Thread(futureTask,"CC").start();
+
+        System.out.println(futureTask.get());//放在最后一行
+        System.out.println(Thread.currentThread().getName()+"结束信息");
 
     }
 }
