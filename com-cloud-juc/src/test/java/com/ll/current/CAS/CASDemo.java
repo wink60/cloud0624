1970afaa00dd5f5cfdd2caf30988a780e7d300bf
wink60 <wink60@163.com>
2020-08-06 08:37:01 +0800

    CAS 操作原理

--- com-cloud-juc/src/test/java/com/ll/current/CAS/CASDemo.java
+++ com-cloud-juc/src/test/java/com/ll/current/CAS/CASDemo.java
@@ -8,6 +8,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  * @ Description：${CAS 是什么？？
  *  CAS 是计算机底层的一个原语，用在多线程操作CPU的时候为了保证线程的安全比较并交换
  *  为了保证多线程的可见性资源参数被valite 修饰
+ *  cas 操作不允许被打断
  * }
  * @ Modified By：
  * @Version: $1.00$
@@ -15,8 +16,10 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class CASDemo {
     public static void main(String[] args) {
         AtomicInteger atomicInteger = new AtomicInteger(5);
+
         atomicInteger.compareAndSet(5,2024);
         atomicInteger.compareAndSet(5,1024);
+
         atomicInteger.getAndIncrement();//a++
         System.out.println("++++++++"+atomicInteger);
         System.out.println( atomicInteger.getAndIncrement());
