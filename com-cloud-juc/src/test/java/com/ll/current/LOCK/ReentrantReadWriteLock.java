29c2c4b08986b27f86ecf35333570dfdda8eb5e6
wink60 <wink60@163.com>
2020-08-08 21:18:36 +0800

    ReentranReadWrite 读写锁

---
+++ com-cloud-juc/src/test/java/com/ll/current/LOCK/ReentrantReadWriteLock.java
@@ -0,0 +1,19 @@
+package com.ll.current.LOCK;
+
+/**
+ * @ Author     ：liul
+ * @ Date       ：Created in 15:24 2020/8/7
+ * @ Description：${
+ *     独占锁（写锁）/共享锁（读锁）/互斥锁
+ *   读读能共存
+ *   读写不能共存
+ *   写写不能共存
+ *
+ *   写操作：原子+独占=中间不许被分隔
+ * }
+ *  * @ Modified By：
+ * @Version: $1.00$
+ */
+public class ReentrantReadWriteLock {
+
+}
