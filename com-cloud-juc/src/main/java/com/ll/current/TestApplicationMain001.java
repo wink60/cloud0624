6132d1163349059a997d314a789b3bac13c86720
wink60 <wink60@163.com>
2020-09-02 14:16:34 +0800

    update

---
+++ com-cloud-juc/src/main/java/com/ll/current/TestApplicationMain001.java
@@ -0,0 +1,22 @@
+package com.ll.current;
+
+import org.junit.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ Author     ：liul
+ * @ Date       ：Created in 15:31 2020/7/5
+ * @ Description：${description}
+ * @ Modified By：
+ * @Version: $1.00$
+ */
+@SpringBootApplication
+public class TestApplicationMain001 {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplicationMain001.class,args);
+    }
+    @Test
+    public void contextLoads() {
+    }
+}
